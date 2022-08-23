79e2f996a79275fa4ae51909bc937fee5b9b62b8
MateusTum <mateustum27@gmail.com>
2022-08-23 12:01:11 -0300

    FIRST CLASSES
    My first classes learning python

---
+++ pythontest.py
@@ -0,0 +1,81 @@
+#==========================VARIABLES===============================
+
+#======================STRINGS A SERIES OF CARC=================
+
+#firt_name = "Bro"
+#second_name =
+#last_name =
+#nickname = 
+#fullname = first_name+" "+second_name+" "+last_name
+#print(type(name))
+#print("Hello"+name)
+
+#=========================INTS==================================
+
+#age = 21
+#BIRTHDAY EXAMPLE
+#age += 1
+#print(age)
+#print(type(age))
+#print ("Your age is: "+str(age))  #if you need to display a string with a data you have to type the +str(age) thing
+
+
+#=======================FLOAT DATA TYPE================
+
+#height = 250.5 
+#print("Your height is: "+str(height)+"cm")
+#print(type(height))
+
+#====================BOOLEAN DATA TYPE ONLY STORE TRUE OR FALSE===============
+#good use to see if something is true
+
+#human = False
+#print("Are you a human: "+str(human))
+#print(type(human))
+
+
+#MULTIPLE ASSIGNMENT = ALLOWS US TO ASSIGN MULTIPLE VARIABLES AT THE SAME TIME IN ONE LINE OF CODE
+
+#name = "Matt"
+#age = 22
+#attractive = True
+
+#name, age, attractive = "Matt", 21, True
+
+#print(name), print(age), print(attractive)
+
+#============== STRING METHODS ====================
+
+#name = "bro code"
+
+#print(len(name))
+#print(name.find("B"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())   #CHECK TO SEE IF STRING CONTAINS ONLY ALPHABETIC LETTERS
+#print(name.count("o"))
+#print(name.replace("o" , "a"))
+#print(name*3)
+
+#=============TYPE CASTING = CONVERT THE DATA TYPE OF A VALUE TO ANOTHER DATA TYPE
+
+x = 1         #int
+y = 2.0       #float
+z = "3"       #str
+
+print(x)
+print(int(y))   #display Y as an integer
+print(z)
+
+z = int(z)   #makes the x an integer
+
+x = float(x)   #makes the z a float 
+
+#if you need to print a variable along a string
+
+
+
+
+
